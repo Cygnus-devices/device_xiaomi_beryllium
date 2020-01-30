1c7296286b6a827bd9c99997215f00b6998f83fa
dhruv <dhruvgera61@gmail.com>
2020-01-30 14:13:49 +0000

    Cygnus bring-up

--- cygnus_beryllium.mk
+++ cygnus_beryllium.mk
@@ -6,12 +6,12 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Havoc stuff.
+# Inherit some common CygnusOS stuff.
 TARGET_INCLUDE_WIFI_EXT := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := cygnus_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
