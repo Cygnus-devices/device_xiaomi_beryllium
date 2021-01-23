ef5bb0f0eb2c55f180edd009fdeb33da4ede737c
Dhruv <dhruvgera61@gmail.com>
2021-01-23 18:03:37 +0100

    beryllium: Bring-up for cygnus
    Signed-off-by: Dhruv <dhruvgera61@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_beryllium.mk
+    $(LOCAL_DIR)/cygnus_beryllium.mk
--- cygnus_beryllium.mk
+++ cygnus_beryllium.mk
@@ -6,14 +6,13 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common CygnusOS stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/cygnus/configs/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_beryllium
+PRODUCT_NAME := cygnus_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
