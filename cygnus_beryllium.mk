7a58705fa561e1265935f0be9663a36407837e59
Dhruv Gera <dhruvgera61@gmail.com>
2021-02-02 01:37:58 +0530

    beryllium: Switch to Redfin fp

--- cygnus_beryllium.mk
+++ cygnus_beryllium.mk
@@ -21,7 +21,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 WITH_GAPPS := true
 CYGNUS_BUILD_TYPE := OFFICIAL
 
-BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.2.0.QEJMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.2.0.QEJMIXM release-keys" \
